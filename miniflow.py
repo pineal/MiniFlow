01a6f55faf9d46526b27f2d140dec8d0e848f320
hesen <hesen.zhang@gmail.com>
2017-08-19 01:51:15 -0400

    Implemented sigmoid forward function and MSE cost function

--- miniflow.py
+++ miniflow.py
@@ -149,3 +149,58 @@ class Linear(Node):
         W = self.inbound_nodes[1].value
         b = self.inbound_nodes[2].value
         self.value = np.dot(X, W) + b
+
+"""
+class Sigmoid: 
+    Conceptually the sigmoid function makes decisions.
+    When given weighted features from some data, it indicates whether or not
+    the features contribure to a classfication.
+    In that way, a sigmoid activation works well forwarding a linear transformation.
+"""
+class Sigmoid(Node):
+    def __init__(self, node):
+        Node.__init__(self, [node])
+
+    def _sigmoid(self, x):
+        """
+        Will be used in both 'forward' and 'backward'
+        Args:
+            x: A numpy array-like object
+        Return:
+            result of sigmoid function - apply the step function
+        """
+        return 1. / (1. + np.exp(-x))
+
+    def forward(self):
+        self.value = self._sigmoid(self.inbound_nodes[0].value)
+
+"""
+class MSE:
+    squarded error cost function
+    should be used as the last node for a network
+"""
+
+class MSE(Node):
+    def __init__(self, y, a):
+        Node.__init__(self, [y, a])
+
+
+    def forward(self):
+    """
+    Calculated the mean squared error.
+    """
+    # NOTE: We reshape these to avoid possible matrix/vector broadcast errors.
+    # For example: if we subtract an array of shape (3,) from an array of shape (3, 1)
+    # we get an array of shape(3,3) as the result when we want an array of shape (3, 1) instead
+    #
+    # Making both arrays (3,1) insures the result is (3,1) and does
+    # an elementwise subtraction as expeceted.
+
+    y = self.inbound_nodes[0].value.reshape(-1, 1)
+    a = self.inbound_nodes[1].value.reshape(-1, 1)
+    m = self.inbound_node[0].value.shape[0]
+
+    self.value = np.mean((y - a)**2)
+
+
+
